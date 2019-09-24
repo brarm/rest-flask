20b279d394752fca27bb529ae2fa5efd2858fa6e
Mehtaab Brar <mehtaab.brar@gmail.com>
2019-09-24 03:32:22 -0500

    Left the rc off .bashrc

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ curl https://pyenv.run | bash
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bash
+source ~/.bashrc
 
 pyenv update
 
